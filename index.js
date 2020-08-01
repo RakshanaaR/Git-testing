7bb66dd992bfaa8429c6f01201d41c9a188b1ab0
Rakshanaa R <rakshanaa17124@cse.ssn.edu.in>
2020-08-02 01:01:46 +0530

    changed rect.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var rect = require('./rectangle');
-function solveRect(l,b)
-{
-    console.log("solving for rectangle with l = "+l+"and b ="+b);
-    if(l <= 0 || b <= 0)
-    {
-        console.log("Rect dimensions greater than zero");
-    }
-    else{
-        console.log("Area = "+rect.area(l,b));
-        console.log("Perimeter = "+rect.perimeter(l,b));
-    }
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
 
-}
-solveRect(5,12);
-solveRect(0,1);
-solveRect(-2.-3);
+solveRect(2,4);
+solveRect(0,-1);
